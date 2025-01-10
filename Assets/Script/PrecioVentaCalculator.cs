117e9cd147ffc70d944255357aa493b7d898375a
MapeMC <michaellipe@hotmail.com>
2025-01-10 10:04:52 -0500

    first commit

---
+++ Assets/Script/PrecioVentaCalculator.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float costo, utilidad, CIF, utilidadEsperada, materiaPrima, manoObra, precioVenta;
+    
+    [SerializeField] TextMeshPro textMeshPro;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
